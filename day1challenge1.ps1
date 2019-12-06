ee09aa8c871dfcac14959764a21d0da42fecf74d
Seth Stuart <sstuart-c@marchex.com>
2019-12-06 10:22:01 -0800

    Optimized D1C1

--- day1challenge1.ps1
+++ day1challenge1.ps1
@@ -1,17 +1,10 @@
 Function TextToPSList {
     $list = get-content -path .\day1input.txt
 	$fuelReq = @()
-    #write-host "$list[0]"
-	foreach($mass in $list) {
-	    $m = ($mass / 3) - 2
-		$m = [math]::floor($m)
-		$fuelReq += $m
-	}
 	$totalReq = 0
-	foreach($req in $fuelReq) {
-	    $totalReq = $totalReq + $req
+	foreach($mass in $list) {
+		$totalReq += ([math]::floor($mass/3)-2)
 	}
 	$totalReq
 }
-
 TextToPSList
